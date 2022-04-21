3b8a17f04299152bafdd11465023a9e7f32c0745
zefirlover <71660586+zefirlover@users.noreply.github.com>
2022-04-21 13:10:39 +0300

    deleting part 2

--- test/pages/explore.page.js
+++
@@ -1,7 +0,0 @@
-class ExplorePage {
-    get topicsRef() {
-        return $('//*[@id="js-pjax-container"]/div[1]/nav/div/a[2]');
-    }
-}
-
-module.exports = new ExplorePage();
--- test/pages/forgotPassword.page.js
+++
@@ -1,7 +0,0 @@
-class ForgotPasswordPage {
-    get captcha() {
-        return $('[style="height: 311px; width: 462px;"]');
-    }
-}
-
-module.exports = new ForgotPasswordPage();
