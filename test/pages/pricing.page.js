695f5d51888efc688b896c9823b85443b2903054
zefirlover <ibodikmushta@gmail.com>
2022-04-22 14:06:36 +0000

    Update from https://github.com/zefirlover/QA-test-task/commit/9f731e0b7bccedbb1255df56995d18937db91daf

--- test/pages/pricing.page.js
+++
@@ -1,7 +0,0 @@
-class PricingPage {
-    get joinButton() {
-        return $('/html/body/div[4]/main/div[2]/div[2]/div[1]/div[1]/div/div/div/div[4]/a');
-    }
-}
-
-module.exports = new PricingPage();
--- test/pages/topics.page.js
+++
@@ -1,7 +0,0 @@
-class TopicsPage {
-    get topicsH1() {
-        return $('.h1');
-    }
-}
-
-module.exports = new TopicsPage();
